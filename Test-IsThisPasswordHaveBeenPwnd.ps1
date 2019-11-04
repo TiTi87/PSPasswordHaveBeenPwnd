6c505fa9acee1a7e6fb9c6011d6b9013e5b491b9
Etienne L <TiTi87@users.noreply.github.com>
2019-11-04 11:28:21 +0100

    Update Test-IsThisPasswordHaveBeenPwnd.ps1
    Added Tests (Pester)

--- Test-IsThisPasswordHaveBeenPwnd.ps1
+++ Test-IsThisPasswordHaveBeenPwnd.ps1
@@ -1,3 +1,4 @@
+#Requires -Modules Pester
 Function Get-StringHash([String] $String, $HashName="MD5"){
 <#
 .SYNOPSIS 
@@ -45,3 +46,44 @@ Function Test-IsThisPasswordHaveBeenPwnd ([String] $Password) {
     Write-Host  "You're Password Seems Fine" 
     return $false; 
 }
+
+
+Describe "Test-IsThisPasswordHaveBeenPwnd" {
+  Context "When Password is 'Passw0rd123456'" {
+    Mock Invoke-RestMethod {
+            return "00EA5B4A1F59DDC8296ED29F24BBFCD8F43:1
+263AFCD372802C5BC5E71A0FC93B70A9828:1
+26850297C3AF234196271AFA1DFF7D9BCDC:1
+26B9743E72128CDE3370C992A86B49E011E:5
+26D17F2E7CEC134698A864C43E5CFBD84ED:50
+26F2ECA536A2752D509258781BE4FD08F6A:23
+2722CE42DBE70685BBAC0EFADB5E76D98F2:2
+276222744A017CFB757924F76814C3C5820:3
+2772FF587C44A48B0850F192189EC1CD182:1
+27CB1DA8A3EBE5D50F05128739BE0DF287F:49
+280D579AB5A7974B27005623093FFEBBC69:5"
+    }
+    it "find that the password have been powned, at least 23 time" {
+        Test-IsThisPasswordHaveBeenPwnd -Password 'Passw0rd123456' | should be $true
+    }
+  }
+  Context "When Password is 'Correct Horse Battery Staple'" {
+    Mock Invoke-RestMethod {
+            return "1112E7514BC4A89C7F300675BC5D178122C:1
+11F509408B11336BFDBEFE9686E1E7A3C7D:2
+12235B5CADCB7712B3FEF972CE597A9C51E:1
+123473FCCE58D358A16005E731783255FAA:2
+137EECE2E17D54388DFB36537FE1DF894E0:1
+1383F497EF7CA2164C6681A0D0578827D20:44
+13A901CCD6B55903D58C6CC6B9348DEBF21:39
+13F5B7D74DE3744F9DEA8C6E4224BD683B5:1
+140F91FE3EADCD217647061D07BB15A776F:2"
+    }
+    it "find that the password have not been powned" {
+        $ret = Test-IsThisPasswordHaveBeenPwnd -Password "Correct Horse Battery Staple"
+        Assert-MockCalled Get-StringHash -Scope It -Time 1
+        $ret | should be $false
+    }
+  }
+}
+
