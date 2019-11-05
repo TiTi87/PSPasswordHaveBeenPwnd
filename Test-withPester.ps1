2540a9a70ba194509aebea0ce0c0f5ddd401d0f5
Etienne L <TiTi87@users.noreply.github.com>
2019-11-05 12:26:04 +0100

    Update Test-withPester.ps1
    added Code Coverage Diag in pester test

--- Test-withPester.ps1
+++ Test-withPester.ps1
@@ -1,3 +1,3 @@
 #requires -module pester
 
-Invoke-Pester -OutputFile "./Result-Pester.XML" -OutputFormat NUnitXML './Test-IsThisPasswordHaveBeenPwnd.ps1'
+Invoke-Pester -OutputFile "./Result-Pester.XML" -OutputFormat NUnitXML -CodeCoverage './Test-IsThisPasswordHaveBeenPwnd.ps1'
